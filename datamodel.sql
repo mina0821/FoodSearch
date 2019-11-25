6997b6ff8f92211fffcbf605e19b1812ed57c951
mina0821 <mingnan0821@gmail.com>
2019-11-25 11:50:47 -0500

    reset password

--- datamodel.sql
+++ datamodel.sql
@@ -21,3 +21,15 @@ create table Review(
 	primary key (name),
 	foreign key (name) references Object (name) on delete cascade
 );
+
+INSERT INTO `object` (`name`, `rating`, `lat`, `longt`) VALUES ('Best Friend Hot Pot', '4.2', '43.59', '-79.59');
+INSERT INTO `object` (`name`, `rating`, `lat`, `longt`) VALUES ('Chinese Legend Hot Pot', '1.8', '43.29', '-79.19');
+INSERT INTO `object` (`name`, `rating`, `lat`, `longt`) VALUES ('Happy Lamb Hot Pot', '4.7', '43.57', '-79.66');
+INSERT INTO `object` (`name`, `rating`, `lat`, `longt`) VALUES ('Kim Tao Hot Pot', '2.7', '43.85', '-79.38');
+INSERT INTO `object` (`name`, `rating`, `lat`, `longt`) VALUES ('Morals Hot Pot', '3.9', '43.55', '-79.61');
+
+INSERT INTO `review` (`name`, `review`) VALUES ('Best Friend Hot Pot', 'Nice place to eat.');
+INSERT INTO `review` (`name`, `review`) VALUES ('Chinese Legend Hot Pot', 'Good place to eat.');
+INSERT INTO `review` (`name`, `review`) VALUES ('Happy Lamb Hot Pot', 'Excellent place to eat.');
+INSERT INTO `review` (`name`, `review`) VALUES ('Kim Tao Hot Pot', 'Good food.');
+INSERT INTO `review` (`name`, `review`) VALUES ('Morals Hot Pot', 'Nice food.');
--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=db-rst", "root", "");
+    $conn = new PDO("mysql:host=localhost;dbname=db-rst", "root", "root");
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
